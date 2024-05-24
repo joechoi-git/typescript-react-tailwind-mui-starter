8d80f48f8824fffb962f847f56ae11b6f550af4b
Joe Choi <joechoi.newyork@gmail.com>
2024-05-24 06:11:03 -0400

    update

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -125,7 +125,7 @@ export default function Dashboard() {
                             noWrap
                             sx={{ flexGrow: 1 }}
                         >
-                            <span className="text-3xl underline">Hello world!!</span>
+                            <span className="text-3xl underline">Dashboard</span>
                         </Typography>
                         <IconButton color="inherit">
                             <Badge badgeContent={4} color="secondary">
