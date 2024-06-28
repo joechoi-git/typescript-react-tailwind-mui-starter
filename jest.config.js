666850382261850fd125d52106fb1f67a5ad4a50
Joe Choi <joechoi.newyork@gmail.com>
2024-06-28 14:40:18 -0400

    updated jest configuration

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,13 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "jsdom",
-    setupFilesAfterEnv: ["<rootDir>/src/setupTests.tsx"]
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.tsx"],
+    transform: {
+        "^.+\\.(ts|tsx|js|jsx)$": "ts-jest"
+    },
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+            "<rootDir>/src/__mocks__/file.js",
+        "\\.(css)$": "identity-obj-proxy"
+    }
 };
