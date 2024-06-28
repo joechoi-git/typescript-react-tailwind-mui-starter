186b58911efcfd5b9a29808c3dc6426eae599ed9
Joe Choi <joechoi.newyork@gmail.com>
2024-06-28 14:03:51 -0400

    test

--- src/setupTests.tsx
+++ src/setupTests.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/matchers";
 import "@testing-library/jest-dom/jest-globals";
@@ -5,3 +6,7 @@ import "@testing-library/jest-dom/jest-globals";
 jest.mock("@mui/x-charts", () => ({
     BarChart: jest.fn().mockImplementation(({ children }) => children)
 }));
+
+jest.mock("./Chart", () => {
+    return jest.fn(() => <div>Mock Chart</div>);
+});
